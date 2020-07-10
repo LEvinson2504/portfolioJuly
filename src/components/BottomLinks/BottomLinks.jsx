3bb38e41dccb19693538c5b27c558a39fd71a842
LEvinson2504 <ldsouza2504@gmail.com>
2020-07-10 13:42:09 +0530

    added linkedin

--- src/components/BottomLinks/BottomLinks.jsx
+++ src/components/BottomLinks/BottomLinks.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import {
   InstagramOutlined,
   GithubOutlined,
-  TwitterOutlined,
+  LinkedinOutlined,
 } from "@ant-design/icons";
 import "./BottomLinks.module.css";
 const BottomLinks = () => {
-  const iconStyle = { fontSize: "50px", color: "#08c" };
+  const iconStyle = { fontSize: "50px", color: "#2867b2" };
   return (
     <>
       <a href="https://www.instagram.com/levinson_dsouza/">
         <InstagramOutlined style={{ fontSize: "50px", color: "#c13584" }} />
       </a>
-      <a href="https://www.instagram.com/levinson_dsouza/">
+      <a href="https://github.com/LEvinson2504">
         <GithubOutlined style={{ fontSize: "50px", color: "black" }} />
       </a>
-      <a href="https://www.instagram.com/levinson_dsouza/">
-        <TwitterOutlined style={iconStyle} />
+      <a href="https://www.linkedin.com/in/levinson-d-souza-093b61129/">
+        <LinkedinOutlined style={iconStyle} />
       </a>
     </>
   );
